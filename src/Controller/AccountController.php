a268bc2696aae1cd579b80912e55567ebc899a21
Devzair <contact@devzair.fr>
2025-12-28 19:06:23 +0100

    ✨ feat(security): ajouter authentification et gestion de compte
    - implémenter un contrôleur de sécurité avec des routes pour la connexion et la déconnexion
    - ajouter un contrôleur de compte pour gérer les informations utilisateur
    - intégrer un authentificateur personnalisé pour la connexion
    - configurer la gestion des sessions et des cookies pour la connexion persistante
    - créer des templates pour la connexion et l'enregistrement d'utilisateur
    
    ♻️ refactor(security): améliorer la configuration de sécurité
    
    - ajouter des options de déconnexion et de mémorisation dans la configuration de sécurité
    - modifier la route principale pour rediriger vers la page d'accueil après la connexion
    - ajuster le nom de la route de déconnexion pour une meilleure clarté

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class HomeController extends AbstractController
+final class AccountController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
         ]);
     }
 }
